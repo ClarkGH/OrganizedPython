96caf351820fc50f1cb1c591ce0233732048861c
clarkgh <demolisho@hotmail.com>
2018-10-29 11:13:22 -0700

    lists

--- SentDex/syntax.py
+++ SentDex/syntax.py
@@ -1,8 +1,8 @@
 snake_cased_variables = 'snake_cased_variables'
 my_tuple_of_languages = ("Python", "Java", "C++", "C#", "Javascript") # don't need parenths
-
+my_list_of_pies = ['blueberry', 'cherry', 'custard', 'sweet potato']
 # print(type(snake_cased_variables))
 # print(type(my_tuple_of_languages))
 
-for language in my_tuple_of_languages:
+for language in my_list_of_pies:
     print(language)
