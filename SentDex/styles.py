ee221f0ec29c6125cd32fa4b46602a676e2c6cf4
clarkgh <demolisho@hotmail.com>
2018-10-29 11:10:53 -0700

    for loops in python

--- SentDex/styles.py
+++
@@ -1,5 +0,0 @@
-snake_cased_variables = 'snake_cased_variables'
-my_tuple = ("Python", "Java", "C++", "C#", "Javascript") # don't need parenths
-
-print(type(snake_cased_variables))
-print(type(my_tuple))
---
+++ SentDex/syntax.py
@@ -0,0 +1,8 @@
+snake_cased_variables = 'snake_cased_variables'
+my_tuple_of_languages = ("Python", "Java", "C++", "C#", "Javascript") # don't need parenths
+
+# print(type(snake_cased_variables))
+# print(type(my_tuple_of_languages))
+
+for language in my_tuple_of_languages:
+    print(language)
