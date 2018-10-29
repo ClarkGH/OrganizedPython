6511b6cb580231169588c5e7a3fb521fd48caeaf
clarkgh <demolisho@hotmail.com>
2018-10-29 11:07:43 -0700

    tuples

--- SentDex/README.md
+++ SentDex/README.md
@@ -1,3 +1,7 @@
 # Notes following SentDex's python 3 tutorial: 
 
 [Tutorial](https://www.youtube.com/watch?v=eXBD2bB9-RA&list=PLQVvvaa0QuDeAams7fkdcwOGBpGdHpXln)
+
+## To run
+
+1. $ python3 $file_name
---
+++ SentDex/styles.py
@@ -0,0 +1,5 @@
+snake_cased_variables = 'snake_cased_variables'
+my_tuple = ("Python", "Java", "C++", "C#", "Javascript") # don't need parenths
+
+print(type(snake_cased_variables))
+print(type(my_tuple))
